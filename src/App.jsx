90a99dd7827d5e87a6d6d2d9de43c0be27c67b9a
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-03-31 19:23:17 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { Component } from "react";
 
 class App extends Component {
   render() {
-    const total = this.totalFeedback();
-    const { good, neutral, bad } = this.state;
-
-    return <></>;
+    return (
+      <>
+        <div></div>
+      </>
+    );
   }
 }
 
