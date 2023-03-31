c3ed280c78f0acaf3ff3da25a28112b54a25b303
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-03-31 19:19:04 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    const total = this.totalFeedback();
+    const { good, neutral, bad } = this.state;
+
+    return <></>;
+  }
+}
+
+export { App };
